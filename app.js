dae6874136e58ae016af9cd4a1c1a38e86179737
aniede <ndubest56@gmail.com>
2023-02-05 12:19:20 +0100

    getting data from dase

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
 const express = require('express')
 const app = express()
 
-const logger = require('./logger-middle')
-const authorize = require('./my-final/authorize')
+const {people} = require('./data')
 
-app.use([logger, authorize])
-
-//middleware 
 app.get('/', (req, res) => {
-	res.send('<h1>Home Page</h1>')
+	res.status(200).json({success: true, data: people})
 })
 
-app.get('/about', (req, res) => {
-	res.send('<h1> About Page </h1>')
-})
 
-app.get('/api/products', (req, res) => {
-	res.send('<h1> Products Page </h1>')
-})
-
-app.get('/home/items', (req, res) => {
-	res.send('<h1> List of items </h1>')
-})
 app.listen(5000, () => {
-	console.log('listening on port 5000...')
+	console.log('App is listening on port 5000...')
 })
---
+++ my-final/authorise-app
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+const logger = require('./logger-middle')
+const authorize = require('./my-final/authorize')
+
+app.use([logger, authorize])
+app.use(morgan('tiny'))
+
+//middleware 
+app.get('/', (req, res) => {
+	res.send('<h1>Home Page</h1>')
+})
+
+app.get('/about', (req, res) => {
+	res.send('<h1> About Page </h1>')
+})
+
+app.get('/api/products', (req, res) => {
+	res.send('<h1> Products Page </h1>')
+})
+
+app.get('/home/items', (req, res) => {
+	res.send('<h1> List of items </h1>')
+})
+app.listen(5000, () => {
+	console.log('listening on port 5000...')
+})
