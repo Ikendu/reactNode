258bea93576e06ffb4d9b613edf5121c317d1945
aniede <ndubest56@gmail.com>
2023-02-05 09:31:43 +0100

    middleware function

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const exprss = reqiure('express')
+const express = require('express')
 const app = express()
 
+//middleware 
+
+app.get('/', logger, (req, res) => {
+	res.send('<h1>Home Page</h1>')
+})
+
+app.get('/about', logger, (req, res) => {
+	res.send('<h1> About Page </h1>')
+})
+
 app.listen(5000, () => {
-	console.log('listening on port 5000')
+	console.log('listening on port 5000...')
 })
