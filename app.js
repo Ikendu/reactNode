3ba53f7997bd66fc6b1d44da6b0d8426f8197a95
aniede <ndubest56@gmail.com>
2023-02-05 16:20:40 +0100

    first post flavour

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
+let {people} = require('./data')
 
-const {people} = require('./data')
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
 	res.status(200).json({success: true, data: people})
 })
 
+ app.post('/login', (req, res) => {
+ 	const {name} = req.body
+ 	if (name){
+ 		res.status(200).send(`<h2>Welcome ${name}</h2>`)
+ 	}
+ 	res.send('<h2>Please provide a name</h2>')
+ })
 
 app.listen(5000, () => {
 	console.log('App is listening on port 5000...')
